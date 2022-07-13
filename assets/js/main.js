ae3277e381853180cc3a77212b3caec8b35360e1
CarmenXP <xocuaposadas.carmen@gmail.com>
2022-07-12 20:40:06 -0500

    carrito maquetado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 import showProducts from './components/productos.js';
 
+//nodos
+let closeButton = document.querySelector(".cart-close");
+let cartButton = document.querySelector(".count");
+let showCart = document.querySelector(".shopping-cart");
+
 
 document.addEventListener("DOMContentLoaded", () =>{
+    //función para mostrar las cards en página principal
     showProducts()
-    console.log("estoy funcionando")
+
+    //función para abrir y cerrar carrito
+    cartButton.addEventListener("click", ()=>{
+        showCart.classList.add("show")
+    });
+    
+    closeButton.addEventListener("click", ()=>{
+        showCart.classList.remove("show")
+    });
+    
 })
